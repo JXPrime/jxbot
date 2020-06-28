f45753f621d91fd52adbabd123fd83d3ddb3e913
Jonas Bush <jonasbush@gmail.com>
2020-06-27 17:23:11 -0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js')
+const client = new Discord.Client() 
+const fs = require('fs');
+const readline = require('readline');
+const _ = require('underscore');
+
+const smilies = [];
+
+async function processSmilies(){
+  const fileStream = fs.createReadStream('./data/keyfile.txt');
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  var i = 0;
+  for await (const line of rl)
+  {
+    var parts = line.split(/,/);
+    var obj = {
+      smilie: parts[0],
+      location: './data/' + parts[1].trim()
+    };
+    
+    smilies.push(obj);
+    ++i;
+  }
+  console.log('loaded ' + smilies.length + ' smilies');
+}
+
+processSmilies();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+}) 
+
+client.on('message', message => {
+  var theSmilie = _.find(smilies, function(val) {
+    return message.content == val.smilie;
+  });
+
+  if(!theSmilie)
+    return;
+    
+
+  const exampleEmbed = new Discord.MessageAttachment(theSmilie.location);
+
+  message.channel.send(exampleEmbed);
+})
+
+client.login('NjQwMzYyMzk3NjY3ODE5NTIw.XvfTyQ.hJ4LoZHszNnrh-LZ9NhZvLhZo7M')
